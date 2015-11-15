c9d4224e223a1e62cd8921b0ef40cc0ccaad6046
MalusGreen <kevin97illu@gmail.com>
2015-11-14 20:01:59 -0500

    Enemy Class - PROTO
    Behavior:
    The AI logic of the enemy. If an action should happen
    the boolean array actions[] will be updated.
    Action:
    The implementation of the action that a enemy takes.
    
    PROTO - Prototype of a basic enemy.

---
+++ Game/core/src/com/mygdx/game/Enemy.java
@@ -0,0 +1,33 @@
+package com.mygdx.game;
+
+/**A generic Enemy superclass. All Enemy(s) are Ship(s).
+ *
+ * Created by Feng on 11/14/2015.
+ */
+public abstract class Enemy extends Ship {
+    boolean[] actions;
+
+    /**Initiates the location of an enemy and is the constructor of the Generic Enemy superclass.
+     *
+     * This constructor should never be called in isolation.
+     *
+     * @param x X - Cord
+     * @param y Y - Cord
+     */
+    protected Enemy(int x, int y){
+        super();
+    }
+
+    /**The Logic/AI of the enemy ship.
+     * Mutates the boolean array actions.
+     */
+    protected abstract void behavior();
+
+    /**The implementation of individual enemy action.
+     * Updates the enemy based on the boolean array actions.
+     *      if     (actions[0]){...}
+     *      else if(actions[1]){...}
+     *      else if(actions[2]){...}
+     */
+    protected abstract void action();
+}
---
+++ Game/core/src/com/mygdx/game/Proto.java
@@ -0,0 +1,33 @@
+package com.mygdx.game;
+
+
+/**Prototype of a basic enemy ship type.
+ * Created by Feng on 11/14/2015.
+ */
+public class Proto extends Enemy{
+    /**Creates a new Proto at points x,y.
+     *
+     * @param x X - Cord
+     * @param y Y - Cord
+     */
+    public Proto(int x, int y){
+        super(x,y);
+    }
+
+    
+    @Override
+    public void move(){
+        //TODO implement
+    }
+
+    @Override
+    protected void behavior(){
+        //TODO implement
+    }
+
+    @Override
+    protected void action(){
+        //TODO implement
+    }
+
+}
