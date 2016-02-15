ddc57c108b8239879f3784bb09bac981db65ce01
Hongyu Wang <hongywang@hotmail.com>
2016-02-15 15:20:17 -0500

    AlmanacState creation. This will be the abstract class from which all almanac pages will inherit from

---
+++ Game/core/src/gamestates/almanacState/AlmanacState.java
@@ -0,0 +1,13 @@
+package gamestates.almanacState;
+
+/**
+ * Created by Hongyu Wang on 1/25/2016.
+ */
+public abstract class AlmanacState {
+    protected String file_name;
+    public AlmanacState(){
+
+    }
+
+
+}
