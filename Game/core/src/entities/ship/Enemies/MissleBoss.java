4c890392dbfaf4ff21f40a9edd92f621cdfefe59
MalusGreen <kevin97illu@gmail.com>
2016-02-15 15:49:38 -0500

    New Enemy Bosses

---
+++ Game/core/src/entities/ship/Enemies/MissleBoss.java
@@ -0,0 +1,22 @@
+package entities.ship.Enemies;
+
+import gamestates.playState.GameStage;
+
+/**
+ * Created by Kevin Zheng on 2016-02-15.
+ */
+public class MissleBoss extends Enemy {
+    public MissleBoss(int x, int y, GameStage gs){
+        super(x, y, "Proto.png", gs);
+    }
+
+    @Override
+    public void aiPlan(){
+
+    }
+
+    @Override
+    public void aiAct(){
+
+    }
+}
