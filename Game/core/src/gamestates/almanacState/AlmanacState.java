9761a62ec7ea7b7c735b88b5311a141ccb8e803b
Hongyu Wang <hongywang@hotmail.com>
2016-02-15 16:08:10 -0500

    AlmanacState creation. This will be the abstract class from which all almanac pages will inherit from

--- Game/core/src/gamestates/almanacState/AlmanacState.java
+++ Game/core/src/gamestates/almanacState/AlmanacState.java
@@ -1,12 +1,18 @@
 package gamestates.almanacState;
 
+import entities.managers.GameStateManager;
+import gamestates.GameState;
+import gamestates.almanacState.components.Component;
+
 /**
  * Created by Hongyu Wang on 1/25/2016.
  */
-public abstract class AlmanacState {
+public abstract class AlmanacState extends GameState {
     protected String file_name;
-    public AlmanacState(){
-
+    protected Component [] asset;
+    public AlmanacState(String file_name, GameStateManager gsm){
+        super(gsm);
+        this.file_name = file_name;
     }
 
 
